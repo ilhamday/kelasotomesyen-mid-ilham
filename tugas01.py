e1830a0db63fffb143268dc79101c5ebb2989bb6
ilhamday <ilhamyudha00@gmail.com>
2023-02-02 20:38:44 +0700

    Add requirement files and change variables requirements.txt x variable to Url title to Title add some comments

--- tugas01.py
+++ tugas01.py
@@ -2,12 +2,14 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
+# List url yang perlu diakses
 Urls = ["tiket.com", "tokopedia.com", "orangsiber.com", "demoqa.com", "automatetheboringstuff.com"]
 
-for x in Urls:
-    driver.minimize_window()
-    driver.get("https://{}".format(x))
-    title = driver.title
-    print(f"{x} - {title}")    
 
-driver.close()
+for Url in Urls:
+    driver.minimize_window()    # minimize window sejak awal
+    driver.get(f"https://{Url}") # akses ke url yang sudah ditentukan
+    Title = driver.title # menyimpan title website ke variable Title
+    print(f"{Url} - {Title}")  # Url dari list + Title yang didapatkan
+
+driver.close() # keluar dari window
